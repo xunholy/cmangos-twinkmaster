3c516e634115e7ad49ef17f017aa06e46c5333c4
Michael Fornaro <20387402+xUnholy@users.noreply.github.com>
2026-03-03 18:48:18 +1100

    Fix vendor item IDs verified against live database
    Corrected item IDs that resolved to wrong items in CMaNGOS Classic DB:
    - Wingblade: 5196 -> 6504
    - Twisted Chanter's Staff: 7429 -> 890
    - Deviate Scale Gloves: 5194 -> 6467
    - Gold-plated Buckler: 5195 -> 5443
    - Feet of the Lynx: 2585 -> 1121
    - Commander's Crest (lvl 23): replaced with Redbeard Crest (12997, lvl 19)
    
    Removed items exceeding level 19 RequiredLevel:
    - Free Action Potion (5634, req lvl 20)
    - Limited Invulnerability Potion (3387, req lvl 45)
    - Razor Arrow (3030, req lvl 25)
    - Solid Shot (3033, req lvl 25)
    
    Added replacements:
    - Deviate Scale Belt (6468), Elixir of Lesser Agility (3390),
      Elixir of Defense (3389), Elixir of Firepower (6373),
      Scroll of Protection II (1478), Crafted Heavy Shot (8068)

--- sql/install/world/world.sql
+++ sql/install/world/world.sql
@@ -40,21 +40,21 @@ CREATE TABLE `custom_twinkmaster_original_prices` (
 INSERT IGNORE INTO `custom_twinkmaster_original_prices` (`entry`, `original_buy_price`, `original_sell_price`)
 SELECT `entry`, `BuyPrice`, `SellPrice` FROM `item_template`
 WHERE `entry` IN (
-  1482, 5191, 6448, 6469, 7230, 7429, 6472, 5196, 6505,
-  2059, 10399, 5404, 5195, 6320, 2585, 4385, 6087, 5194,
+  1482, 5191, 6448, 6469, 7230, 890, 6472, 6504, 6505,
+  2059, 10399, 5404, 5443, 12997, 1121, 4385, 6087, 6467, 6468,
   4381,
-  5634, 3387, 2459, 3388, 2454, 2455, 929,
-  2515, 3030, 2519, 3033,
+  2459, 3388, 2454, 2455, 929, 3390, 3389, 6373, 1478,
+  2515, 2519, 8068,
   2581, 3531
 );
 
 UPDATE `item_template` SET `BuyPrice` = 0, `SellPrice` = 0
 WHERE `entry` IN (
-  1482, 5191, 6448, 6469, 7230, 7429, 6472, 5196, 6505,
-  2059, 10399, 5404, 5195, 6320, 2585, 4385, 6087, 5194,
+  1482, 5191, 6448, 6469, 7230, 890, 6472, 6504, 6505,
+  2059, 10399, 5404, 5443, 12997, 1121, 4385, 6087, 6467, 6468,
   4381,
-  5634, 3387, 2459, 3388, 2454, 2455, 929,
-  2515, 3030, 2519, 3033,
+  2459, 3388, 2454, 2455, 929, 3390, 3389, 6373, 1478,
+  2515, 2519, 8068,
   2581, 3531
 );
 
@@ -64,40 +64,42 @@ WHERE `entry` IN (
 DELETE FROM `npc_vendor` WHERE `entry` = @Entry;
 INSERT INTO `npc_vendor` (`entry`, `item`, `maxcount`, `incrtime`) VALUES
 -- Weapons
-(@Entry, 1482, 0, 0),   -- Shadowfang (1H Sword, BoE)
-(@Entry, 5191, 0, 0),   -- Cruel Barb (1H Sword)
-(@Entry, 6448, 0, 0),   -- Tail Spike (Dagger)
-(@Entry, 6469, 0, 0),   -- Venomstrike (Bow)
-(@Entry, 7230, 0, 0),   -- Smite's Mighty Hammer (2H Mace)
-(@Entry, 7429, 0, 0),   -- Twisted Chanter's Staff (Staff)
-(@Entry, 6472, 0, 0),   -- Stinging Viper (1H Mace)
-(@Entry, 5196, 0, 0),   -- Wingblade (1H Sword)
-(@Entry, 6505, 0, 0),   -- Crescent Staff (Staff)
+(@Entry, 1482,  0, 0),  -- Shadowfang (1H Sword, BoE)
+(@Entry, 5191,  0, 0),  -- Cruel Barb (1H Sword)
+(@Entry, 6448,  0, 0),  -- Tail Spike (Dagger)
+(@Entry, 6469,  0, 0),  -- Venomstrike (Bow)
+(@Entry, 7230,  0, 0),  -- Smite's Mighty Hammer (2H Mace)
+(@Entry, 890,   0, 0),  -- Twisted Chanter's Staff (Staff)
+(@Entry, 6472,  0, 0),  -- Stinging Viper (1H Mace)
+(@Entry, 6504,  0, 0),  -- Wingblade (1H Sword)
+(@Entry, 6505,  0, 0),  -- Crescent Staff (Staff)
 -- Armor
-(@Entry, 2059, 0, 0),   -- Sentry Cloak (Back)
+(@Entry, 2059,  0, 0),  -- Sentry Cloak (Back)
 (@Entry, 10399, 0, 0),  -- Blackened Defias Armor (Leather Chest)
-(@Entry, 5404, 0, 0),   -- Serpent's Shoulders (Leather Shoulders)
-(@Entry, 5195, 0, 0),   -- Gold-plated Buckler (Shield)
-(@Entry, 6320, 0, 0),   -- Commander's Crest (Shield)
-(@Entry, 2585, 0, 0),   -- Feet of the Lynx (Leather Feet)
-(@Entry, 4385, 0, 0),   -- Green Tinted Goggles (Leather Head)
-(@Entry, 6087, 0, 0),   -- Chausses of Westfall (Mail Legs)
-(@Entry, 5194, 0, 0),   -- Deviate Scale Gloves (Leather Gloves)
+(@Entry, 5404,  0, 0),  -- Serpent's Shoulders (Leather Shoulders)
+(@Entry, 5443,  0, 0),  -- Gold-plated Buckler (Shield)
+(@Entry, 12997, 0, 0),  -- Redbeard Crest (Shield)
+(@Entry, 1121,  0, 0),  -- Feet of the Lynx (Leather Feet)
+(@Entry, 4385,  0, 0),  -- Green Tinted Goggles (Leather Head)
+(@Entry, 6087,  0, 0),  -- Chausses of Westfall (Mail Legs)
+(@Entry, 6467,  0, 0),  -- Deviate Scale Gloves (Leather Gloves)
+(@Entry, 6468,  0, 0),  -- Deviate Scale Belt (Leather Waist)
 -- Accessories
-(@Entry, 4381, 0, 0),   -- Minor Recombobulator (Trinket)
+(@Entry, 4381,  0, 0),  -- Minor Recombobulator (Trinket)
 -- Consumables
-(@Entry, 5634, 0, 0),   -- Free Action Potion
-(@Entry, 3387, 0, 0),   -- Limited Invulnerability Potion
-(@Entry, 2459, 0, 0),   -- Swiftness Potion
-(@Entry, 3388, 0, 0),   -- Strong Troll's Blood Potion
-(@Entry, 2454, 0, 0),   -- Elixir of Lion's Strength
-(@Entry, 2455, 0, 0),   -- Minor Mana Potion
-(@Entry, 929,  0, 0),   -- Healing Potion
+(@Entry, 2459,  0, 0),  -- Swiftness Potion
+(@Entry, 3388,  0, 0),  -- Strong Troll's Blood Potion
+(@Entry, 2454,  0, 0),  -- Elixir of Lion's Strength
+(@Entry, 2455,  0, 0),  -- Minor Mana Potion
+(@Entry, 929,   0, 0),  -- Healing Potion
+(@Entry, 3390,  0, 0),  -- Elixir of Lesser Agility
+(@Entry, 3389,  0, 0),  -- Elixir of Defense
+(@Entry, 6373,  0, 0),  -- Elixir of Firepower
+(@Entry, 1478,  0, 0),  -- Scroll of Protection II
 -- Ammo
-(@Entry, 2515, 0, 0),   -- Sharp Arrow
-(@Entry, 3030, 0, 0),   -- Razor Arrow
-(@Entry, 2519, 0, 0),   -- Heavy Shot
-(@Entry, 3033, 0, 0),   -- Solid Shot
+(@Entry, 2515,  0, 0),  -- Sharp Arrow
+(@Entry, 2519,  0, 0),  -- Heavy Shot
+(@Entry, 8068,  0, 0),  -- Crafted Heavy Shot
 -- Bandages
-(@Entry, 2581, 0, 0),   -- Heavy Linen Bandage
-(@Entry, 3531, 0, 0);   -- Heavy Wool Bandage
+(@Entry, 2581,  0, 0),  -- Heavy Linen Bandage
+(@Entry, 3531,  0, 0);  -- Heavy Wool Bandage
